ce322633262f0183d2074c4b2e504c96f75dee48
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2023-03-18 22:14:37 +0200

    Add files via upload

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using BooksAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly APIDbContext context;
+        public BooksController(APIDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Books>>> Get()
+        {
+            return Ok(await context.Books.ToListAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Books>> Get(int id)
+        {
+            var book = await context.Books.FindAsync(id);
+            if (book == null)
+                return BadRequest("Book not found");
+
+            return Ok(book);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Books>>> AddBook(Books book)
+        {
+            context.Books.Add(book);
+            await context.SaveChangesAsync();
+            return Ok(await context.Books.ToListAsync());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<Books>>> UpdateBook(Books request)
+        {
+            var book = await context.Books.FindAsync(request.Id);
+            if (book == null)
+                return BadRequest("Book not found");
+
+            book.Title = request.Title;
+            book.Writer = request.Writer;
+            book.Price = request.Price;
+            await context.SaveChangesAsync();
+            return Ok(await context.Books.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Books>>> DeleteBook(int id)
+        {
+            var book = await context.Books.FindAsync(id);
+            if (book == null)
+                return BadRequest("Book not found");
+
+            context.Books.Remove(book);
+            await context.SaveChangesAsync();
+            return Ok(await context.Books.ToListAsync());
+        }
+    }
+}
