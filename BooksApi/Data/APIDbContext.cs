05463cc6b75ef8dce1175cb40bf82276c2573c79
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2023-03-18 22:09:14 +0200

    Add files via upload

---
+++ BooksApi/Data/APIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BooksAPI.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Books> Books { get; set; }
+    }
+}
